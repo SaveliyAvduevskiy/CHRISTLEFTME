14ccf80bc957da6f51ac5d4a03f9ba2b185382cb
waitletmecook <zzula5152@gmail.com>
2024-10-18 14:37:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from '../src/img/Logo.svg'
+import search from '../src/img/search.svg'
+import cart from '../src/img/cart.svg'
+
+function App() {
+  return (
+    <>
+    ,
+    </>
+  );
+}
+export default App;
